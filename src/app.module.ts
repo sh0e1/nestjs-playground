a602288ee1881f09e5416bc1453d92ffa97f5526
sh0e1 <sh0e1@users.noreply.github.com>
2024-12-29 00:03:10 +0900

    import ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { LoggerMiddleware } from './middleware/logger.middleware';
+import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
+import configuration from './config/configuration';
+import { LoggerMiddleware } from './middleware/logger.middleware';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, CommonModule],
+  imports: [
+    CommonModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
