e75345f602ec57fc128d5720b948f413b2632fd8
sh0e1 <sh0e1@users.noreply.github.com>
2024-12-29 01:13:42 +0900

    validate configuration

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,3 +1,34 @@
-export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
-});
+import { plainToInstance } from 'class-transformer';
+import { IsEnum, IsPort, validateSync } from 'class-validator';
+
+const Environment = {
+  Development: 'development',
+  Production: 'production',
+  Test: 'test',
+};
+type Environment = (typeof Environment)[keyof typeof Environment];
+
+class Configuration {
+  @IsEnum(Environment)
+  env: Environment;
+
+  @IsPort()
+  port: string;
+}
+
+export default () => {
+  const config = {
+    env: process.env.NODE_ENV || Environment.Development,
+    port: process.env.PORT || '3000',
+  };
+
+  const validateConfig = plainToInstance(Configuration, config, {
+    enableImplicitConversion: true,
+  });
+  const errors = validateSync(validateConfig);
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return config;
+};
