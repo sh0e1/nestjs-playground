a69183fc0cd52121fe9dce19d907fd4e3b7e33e9
sh0e1 <sh0e1@users.noreply.github.com>
2025-01-01 20:29:21 +0900

    add accounts table

--- prisma/migrations/20250101112832_create_accounts/migration.sql
+++ prisma/migrations/20250101112832_create_accounts/migration.sql
@@ -1,12 +1,12 @@
 -- CreateTable
-CREATE TABLE `users` (
+CREATE TABLE `accounts` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
     `updatedAt` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
 
-    UNIQUE INDEX `users_email_key`(`email`),
+    UNIQUE INDEX `accounts_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
