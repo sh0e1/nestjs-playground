fc51454cd743a898a920a78f09f7abd83a40466d
sh0e1 <sh0e1@users.noreply.github.com>
2025-01-02 22:01:52 +0900

    implement accounts controller

--- src/controller/accounts/accounts.module.ts
+++ src/controller/accounts/accounts.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AccountModule } from 'src/service/account/account.module';
 
 import { AccountsController } from './accounts.controller';
 
 @Module({
+  imports: [AccountModule],
   controllers: [AccountsController],
 })
 export class AccountsModule {}
