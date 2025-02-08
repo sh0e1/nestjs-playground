618e52e2831682720f330b1d5c5adf5d3653ccdf
sh0e1 <sh0e1@users.noreply.github.com>
2025-02-08 17:56:13 +0900

    fix tests

--- src/controller/accounts/accounts.controller.spec.ts
+++ src/controller/accounts/accounts.controller.spec.ts
@@ -1,4 +1,7 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import configuration from 'src/config/configuration';
+import { AccountServiceModule } from 'src/service/account/account.service.module';
 
 import { AccountsController } from './accounts.controller';
 
@@ -7,6 +10,10 @@ describe('AccountsController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        AccountServiceModule,
+        ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+      ],
       controllers: [AccountsController],
     }).compile();
 
