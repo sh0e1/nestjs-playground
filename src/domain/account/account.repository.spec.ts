e9785ded6fbecd512e51b301e4828b28ce52b8c6
sh0e1 <sh0e1@users.noreply.github.com>
2025-03-16 13:22:49 +0900

    add test cases

--- src/domain/account/account.repository.spec.ts
+++ src/domain/account/account.repository.spec.ts
@@ -5,6 +5,7 @@ import { CommonModule } from 'src/common/common.module';
 import { PrismaService } from 'src/common/prisma/prisma.service';
 import configuration from 'src/config/configuration';
 
+import { RepositoryError } from '../repository.error';
 import { AccountRepository } from './account.repository';
 
 describe('AccountRepository', () => {
@@ -28,22 +29,48 @@ describe('AccountRepository', () => {
     accountRepository = moduleRef.get(AccountRepository);
   });
 
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   describe('findUnique', () => {
-    test('should return an account if the record exists', async () => {
+    test('should return the account if the record exists', async () => {
       const AccountFactory = defineAccountFactory();
-      const account = await AccountFactory.create({
+      const {
+        password: _password,
+        createdAt: _createdAt,
+        updatedAt: _updatedAt,
+        ...expected
+      } = await AccountFactory.create({
         name: 'Test Account',
         email: 'test@example.com',
         password: 'password',
       });
 
-      const res = await accountRepository.findUnique({ id: account.id });
+      const res = await accountRepository.findUnique({ id: expected.id });
       expect(res.isOk()).toBe(true);
-      expect(res._unsafeUnwrap()).toEqual({
-        id: account.id,
-        name: account.name,
-        email: account.email,
-      });
+      expect(res._unsafeUnwrap()).toEqual(expected);
+    });
+
+    test('should return null if the record does not exist', async () => {
+      const res = await accountRepository.findUnique({ id: -1 });
+      expect(res.isOk()).toBe(true);
+      expect(res._unsafeUnwrap()).toBeNull();
+    });
+
+    test('should return the RepositoryError if an error occurs', async () => {
+      const err = new Error('repository error');
+      jest
+        .spyOn(jestPrisma.client.account, 'findUnique')
+        .mockRejectedValue(err);
+
+      const res = await accountRepository.findUnique({ id: -1 });
+      expect(res.isErr()).toBe(true);
+      expect(res._unsafeUnwrapErr()).toStrictEqual(
+        new RepositoryError('failed to find unique for account', {
+          cause: err,
+        }),
+      );
     });
   });
 });
